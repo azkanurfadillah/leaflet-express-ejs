91e3f20b816ca49340f8fd7a94efb4cb155943a1
azkanurf <azkanurfadillah@gmial.com>
2020-06-01 18:48:11 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// index page
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+var port = 8080;
+app.listen(port);
+console.log("Server is running on port", port);
